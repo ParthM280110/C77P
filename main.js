92c328a92a7f9ea6320c9fbb24c00bc66b024fbf
ParthM280110 <135244337+ParthM280110@users.noreply.github.com>
2023-08-16 19:52:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("MyFirst Console Project");
+var firstname="Parth";
+var lastname="Mahajan";
+var fullname=firstname.concat(lastname);
+console.log (fullname);
+var a1=16;
+var a2=8;
+var add=a1+a2;
+console.log (add);
